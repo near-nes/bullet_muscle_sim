3789be43107698ba830e02aec8b7a7bdee91784c
paolo marzolo <pollo.marzo@gmail.com>
2025-08-29 19:37:55 +0000

    feat: load target, removed muscle stuff

--- arm_1dof/bullet_arm_1dof.py
+++ arm_1dof/bullet_arm_1dof.py
@@ -1,9 +1,8 @@
 import os
 
-import arm_1dof.robot_arm_1dof
-import arm_1dof.robot_arm_muscle_1dof
 import numpy as np
-import pybullet
+
+from . import robot_arm_1dof
 
 
 class BulletArm1Dof:
@@ -18,11 +17,16 @@ class BulletArm1Dof:
     URDF_MODEL_FILENAME = "/".join([SDF_MODEL_NAME, "skeleton.urdf"])
     PLANE_MODEL_FILENAME = "/".join([SDF_MODEL_NAME, "plane.urdf"])
 
-    def __init__(self) -> None:
+    def __init__(self, pybullet_instance=None) -> None:
+        if pybullet_instance is None:
+            import pybullet as p
+
+            pybullet_instance = p
         self._server_id = -1
         self._skel_arm = None
         self._skel_arm_id = -1
         self._skel_muscles = None
+        self.p = pybullet_instance
 
     @property
     def server_id(self):
@@ -31,26 +35,26 @@ class BulletArm1Dof:
 
     def InitPybullet(
         self,
+        bullet_connect,
         g=[0.0, 0.0, 0.0],
-        bullet_connect=pybullet.DIRECT,
         sdf_search_path=SDF_SEARCH_PATH,
     ) -> None:
         """Start bullet server and set sdf search path"""
         # Connect to pybullet
-        self._server_id = pybullet.connect(bullet_connect)
+        self._server_id = self.p.connect(bullet_connect)
 
-        pybullet.setGravity(g[0], g[1], g[2], physicsClientId=self._server_id)
-        pybullet.setAdditionalSearchPath(
+        self.p.setGravity(g[0], g[1], g[2], physicsClientId=self._server_id)
+        self.p.setAdditionalSearchPath(
             path=sdf_search_path, physicsClientId=self._server_id
         )
 
-        self._timestep = pybullet.getPhysicsEngineParameters(
+        self._timestep = self.p.getPhysicsEngineParameters(
             physicsClientId=self._server_id
         )["fixedTimeStep"]
 
     def LoadPlane(self, robot_sdf_filename=PLANE_MODEL_FILENAME):
         """Load plane from sdf file"""
-        plane_id = pybullet.loadURDF(
+        plane_id = self.p.loadURDF(
             fileName=robot_sdf_filename,
             useFixedBase=True,
             physicsClientId=self._server_id,
@@ -60,12 +64,12 @@ class BulletArm1Dof:
 
         # Create rotation quaternion for 90 degrees around Z-axis
         # For other axes: X-axis = [1,0,0], Y-axis = [0,1,0], Z-axis = [0,0,1]
-        rotation_quaternion = pybullet.getQuaternionFromEuler(
+        rotation_quaternion = self.p.getQuaternionFromEuler(
             [np.pi / 2, 0, 0]
         )  # 90 deg around Z
 
         # Apply the transformation
-        pybullet.resetBasePositionAndOrientation(
+        self.p.resetBasePositionAndOrientation(
             plane_id, new_position, rotation_quaternion, physicsClientId=self._server_id
         )
 
@@ -74,98 +78,59 @@ class BulletArm1Dof:
 
     def LoadRobot(
         self, robot_sdf_filename=URDF_MODEL_FILENAME
-    ) -> arm_1dof.robot_arm_1dof.RobotArm1Dof:
+    ) -> robot_arm_1dof.RobotArm1Dof:
         """Load robot from sdf file"""
-        self._skel_arm_id = pybullet.loadURDF(
+        self._skel_arm_id = self.p.loadURDF(
             fileName=robot_sdf_filename,
             useFixedBase=True,
             physicsClientId=self._server_id,
         )
 
-        self._skel_arm = arm_1dof.robot_arm_1dof.RobotArm1Dof(
+        self._skel_arm = robot_arm_1dof.RobotArm1Dof(
             bullet_ctrl=self, bullet_body_id=self._skel_arm_id
         )
-
-        return self._skel_arm
-
-    def LoadMuscleRobot(
-        self, robot_sdf_filename=URDF_MODEL_FILENAME
-    ) -> arm_1dof.robot_arm_muscle_1dof.RobotArmMuscle1Dof:
-        """Load robot from sdf file, add muscles"""
-        self._skel_arm_id = pybullet.loadURDF(
-            fileName=robot_sdf_filename,
-            useFixedBase=True,
-            physicsClientId=self._server_id,
-        )
-
-        self._skel_arm = arm_1dof.robot_arm_muscle_1dof.RobotArmMuscle1Dof(
-            bullet_ctrl=self, bullet_body_id=self._skel_arm_id
-        )
-        self._skel_muscles = self._skel_arm.muscles
 
         return self._skel_arm
 
     def LoadTarget(self, target_position, target_color=[1, 0, 0, 1]):
-        ball_shape = pybullet.createVisualShape(
-            shapeType=pybullet.GEOM_SPHERE,
+        ball_shape = self.p.createVisualShape(
+            shapeType=self.p.GEOM_SPHERE,
             radius=0.02,
             rgbaColor=target_color,
             physicsClientId=self._server_id,
         )
-        ball_collision = pybullet.createCollisionShape(
-            shapeType=pybullet.GEOM_SPHERE, radius=0.02, physicsClientId=self._server_id
-        )
-        ball = pybullet.createMultiBody(
+        # ball_collision = self.p.createCollisionShape(
+        #     shapeType=self.p.GEOM_SPHERE,
+        #     radius=0.02,
+        #     physicsClientId=self._server_id,
+        # )
+        ball = self.p.createMultiBody(
             baseMass=0,
             baseInertialFramePosition=[0, 0, 0],
-            baseCollisionShapeIndex=ball_collision,
+            baseCollisionShapeIndex=-1,
             baseVisualShapeIndex=ball_shape,
             basePosition=target_position,
             physicsClientId=self._server_id,
         )
-        return ball
-        # ball_const = pybullet.createConstraint(
-        #     self._skel_arm,
+        # ball_const = self.p.createConstraint(
+        #     self._skel_arm_id,
         #     4,
         #     ball,
         #     -1,
-        #     pybullet.JOINT_FIXED,
+        #     self.p.JOINT_FIXED,
         #     [0, 0, 0],
-        #     pybullet.getJointState(self._skel_arm, 4)[:2],
+        #     self.p.getJointState(self._skel_arm_id, 4)[:2],
         #     [0, 0, 0],
         #     [0, 0, 0],
         #     [0, 0, 0],
         #     physicsClientId=self._server_id,
         # )
+        return ball
+        # return ball, ball_const
 
     def Simulate(self, sim_time) -> None:
         t = 0
         while t < sim_time:
-            pybullet.stepSimulation(physicsClientId=self._server_id)
-
-            if self._skel_muscles:
-                # Update muscle joint states
-                self._skel_arm.UpdateStats()
-                self._skel_muscles.SetJointPos(
-                    JOINT_INDEX(0), self._skel_arm.JointPos(0)
-                )
-                self._skel_muscles.SetJointPos(
-                    JOINT_INDEX(1), self._skel_arm.JointPos(1)
-                )
-                self._skel_muscles.SetJointVel(
-                    JOINT_INDEX(0), self._skel_arm.JointVel(0)
-                )
-                self._skel_muscles.SetJointVel(
-                    JOINT_INDEX(1), self._skel_arm.JointVel(1)
-                )
-
-                self._skel_muscles.Integrate(self._timestep)
-
-                # Apply muscle torques to robot
-                torques = [
-                    self._skel_muscles.GetTorque(JOINT_INDEX(0)),
-                    self._skel_muscles.GetTorque(JOINT_INDEX(1)),
-                ]
-                self._skel_arm.SetJointTorques(joint_ids=[0, 1], torques=torques)
+            self.p.stepSimulation(physicsClientId=self._server_id)
 
             t += self._timestep
